2a70bb8751a7c3612eb484ca634f5ab766bb6b1e
MEGAN-GIT9 <mismeilashvili95@gmail.com>
2026-06-22 17:48:32 +0400

    solved cash problem

---
+++ week 1/cash/cash.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+
+int quarte1(int c);
+int quarte2(int c);
+int quarte3(int c);
+
+int main(void)
+{
+
+    int change, coins;
+
+    do
+    {
+
+        change = get_int("change: ");
+    }
+
+    while (change < 1);
+
+    int coin = quarte1(change);
+    change = change - (25 * coin);
+
+    int mcoin = quarte2(change);
+    change = change - (10 * mcoin);
+
+    int gcoin = quarte3(change);
+    change = change - (5 * gcoin);
+
+    coins = coin + mcoin + gcoin + change;
+
+    printf("%i\n", coins);
+}
+
+int quarte1(int c)
+{
+
+    return c / 25;
+}
+
+int quarte2(int c)
+{
+
+    return c / 10;
+}
+
+int quarte3(int c)
+{
+
+    return c / 5;
+}
